bb43ba866626018bda24cd022b41cdaaff664617
Kataiii <99545244+Kataiii@users.noreply.github.com>
2023-03-24 09:30:02 +0400

    add videoplayer

--- src/widgets/VideoPlayer.tsx
+++ src/widgets/VideoPlayer.tsx
@@ -1,6 +1,7 @@
 import styles from "./css/BlockCard.module.css"
 import Button from "../shared/ui/ButtonPrim";
 import { useNavigate } from "react-router-dom";
+import Video from "../shared/ui/Video";
 
 
 const VideoPlayer = () => {
@@ -9,7 +10,7 @@ const VideoPlayer = () => {
     return(
         <div className={styles.BlockVideoPlayer}>
             <h2 className={styles.TitleVideoPlayer}>Обучающий ролик</h2>
-            <p>Тут будет плеер</p>
+            <Video/>
             <Button title="Попробовать!" onClick={() => navigate('/login')}></Button>
         </div>
     )
