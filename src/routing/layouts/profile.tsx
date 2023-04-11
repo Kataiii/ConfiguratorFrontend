26815898ecfdfd263b67648aacb008b52175170a
Kataiii <99545244+Kataiii@users.noreply.github.com>
2023-04-11 15:44:10 +0400

    add panel for profile

--- src/routing/layouts/profile.tsx
+++ src/routing/layouts/profile.tsx
@@ -2,22 +2,36 @@ import styles from "../../app/App.module.css";
 import AuthorisedHeader from "../../shared/ui/AuthorisedHeader";
 import { Outlet } from "react-router-dom";
 import stylesSidePanel from "./css/DevelopmentArea.module.css"
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 export default function Root(){
+    const location = useLocation();
+    const [styleState, setStyleState] = useState({
+        styleLinkProfile : stylesSidePanel.LinkPersonal,
+        styleLinkLicence : stylesSidePanel.LinkPersonal
+    });
+
+    useEffect(() => {
+        '/home/profile' == location.pathname ? setStyleState({...styleState, styleLinkProfile : stylesSidePanel.LinkPersonalActive}) : setStyleState({...styleState, styleLinkProfile : stylesSidePanel.LinkPersonal})
+        //'/home/profile/licence' == location.pathname ? setStyleState({...styleState, styleLinkLicence : stylesSidePanel.LinkPersonalActive}) : null
+    }, [location])
+
     return(
         <div className={styles.AppDev}>
             <AuthorisedHeader></AuthorisedHeader>
             <div className={stylesSidePanel.MainDiv}>
-                <div className={stylesSidePanel.SidePanelWrap}>
-                    <div>
-                        <Link to={'/home/profile'}>Персональные данные</Link>
-                        <Link to={'/home/profile/licence'}>Подписка</Link>
+                <div className={stylesSidePanel.DivsWrap}>
+                    <div className={stylesSidePanel.SidePanelWrap}>
+                        <div className={stylesSidePanel.SidePanel}>
+                            <Link className={styleState.styleLinkProfile} to={'/home/profile'}>Персональные данные</Link>
+                            <Link className={styleState.styleLinkLicence} to={'/home/profile/licence'}>Подписка</Link>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <Outlet></Outlet>
+                    <div>
+                        <Outlet></Outlet>
+                    </div>
                 </div>
             </div>
         </div>
