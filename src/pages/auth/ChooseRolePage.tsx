6b9b83593cf21a860cb646a4e4058ff62c12a5b2
katya <katya_yuneva@mail.ru>
2024-01-24 18:49:01 +0400

    add refresh function in choose role page

--- src/pages/auth/ChooseRolePage.tsx
+++ src/pages/auth/ChooseRolePage.tsx
@@ -1,16 +1,38 @@
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../..";
 import BlockRoleButton from "../../widgets/BlockRoleButton";
 import styles from "../css/ChooseRolePage.module.css";
+import LoadingPage from "../LoadingPage";
 
 
 const ChooseRoleRage: React.FC = () => {
+    const { store, activeUser } = useContext(Context);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        refreshToken();
+    }, [])
+
+    const refreshToken = async () => {
+        await store.refresh();
+        await activeUser.refreshActiveUser(store.getAccount());
+        setIsLoading(false);
+    }
+
     return (
-        <div className={styles.moduleWrap}>
-            <div className={styles.moduleDiv}>
-                <h1 className={styles.moduleTitle}>Вход в аккаунт</h1>
-                <p className={styles.moduleContent}>Мы нашли у вас несколько аккаунтов, под каким вы хотите зайти?</p>
-                <BlockRoleButton />
-            </div>
-        </div>
+        <>
+            {
+                isLoading
+                    ? <LoadingPage />
+                    : <div className={styles.moduleWrap}>
+                        <div className={styles.moduleDiv}>
+                            <h1 className={styles.moduleTitle}>Вход в аккаунт</h1>
+                            <p className={styles.moduleContent}>Мы нашли у вас несколько аккаунтов, под каким вы хотите зайти?</p>
+                            <BlockRoleButton />
+                        </div>
+                    </div>
+            }
+        </>
     );
 }
 
